d955f79a714e27008c274750aaa483375ec0a62d
Kirill Smelkov <kirr@nexedi.com>
2016-11-16 19:37:02 +0300

    Factor out utility routines to zodbtool.util

---
+++ zodbtool/util.py
@@ -0,0 +1,53 @@
+# zodbtool - various utility routines
+# Copyright (C) 2016  Nexedi SA and Contributors.
+#                     Kirill Smelkov <kirr@nexedi.com>
+
+def ashex(s):
+    return s.encode('hex')
+
+# something that is greater than everything else
+class Inf:
+    def __cmp__(self, other):
+        return +1
+inf = Inf()
+
+
+# get next item from iter -> (item, !stop)
+def nextitem(it):
+    try:
+        item = it.next()
+    except StopIteration:
+        return None, False
+    else:
+        return item, True
+
+# objects of a IStorageTransactionInformation
+def txnobjv(txn):
+    objv = []
+    for obj in txn:
+        assert obj.tid == txn.tid
+        assert obj.version == ''
+        objv.append(obj)
+
+    objv.sort(key = lambda obj: obj.oid)    # in canonical order
+    return objv
+
+# "tidmin..tidmax" -> (tidmin, tidmax)
+class TidRangeInvalid(Exception):
+    pass
+
+def parse_tidrange(tidrange):
+    try:
+        tidmin, tidmax = tidrange.split("..")
+    except ValueError:  # not exactly 2 parts in between ".."
+        raise TidRangeInvalid(tidrange)
+
+    try:
+        tidmin = tidmin.decode("hex")
+        tidmax = tidmax.decode("hex")
+    except TypeError:   # hex decoding error
+        raise TidRangeInvalid(tidrange)
+
+    # empty tid means -inf / +inf respectively
+    # ( which is None in IStorage.iterator() )
+    return (tidmin or None, tidmax or None)
--- zodbtool/zodbcmp.py
+++ zodbtool/zodbcmp.py
@@ -16,38 +16,8 @@ Exit status is 0 if inputs are the same, 1 if different, 2 if error.
 """
 
 from __future__ import print_function
+from zodbtool.util import ashex, inf, nextitem, txnobjv, parse_tidrange, TidRangeInvalid
 from time import time
-
-def ashex(s):
-    return s.encode('hex')
-
-# something that is greater than everything else
-class Inf:
-    def __cmp__(self, other):
-        return +1
-inf = Inf()
-
-
-# get next item from iter -> (item, !stop)
-def nextitem(it):
-    try:
-        item = it.next()
-    except StopIteration:
-        return None, False
-    else:
-        return item, True
-
-# objects of a IStorageTransactionInformation
-def txnobjv(txn):
-    objv = []
-    for obj in txn:
-        assert obj.tid == txn.tid
-        assert obj.version == ''
-        objv.append(obj)
-
-    objv.sort(key = lambda obj: obj.oid)    # in canonical order
-    return objv
-
 
 # compare two storage transactions
 # 0 - equal, 1 - non-equal
@@ -145,26 +115,6 @@ Options:
     -v  --verbose   increase verbosity
     -h  --help      show this help
 """, file=out)
-
-# tidmin..tidmax -> (tidmin, tidmax)
-class TidRangeInvalid(Exception):
-    pass
-
-def parse_tidrange(tidrange):
-    try:
-        tidmin, tidmax = tidrange.split("..")
-    except ValueError:  # not exactly 2 parts in between ".."
-        raise TidRangeInvalid(tidrange)
-
-    try:
-        tidmin = tidmin.decode("hex")
-        tidmax = tidmax.decode("hex")
-    except TypeError:   # hex decoding error
-        raise TidRangeInvalid(tidrange)
-
-    # empty tid means -inf / +inf respectively
-    # ( which is None in IStorage.iterator() )
-    return (tidmin or None, tidmax or None)
 
 def main2():
     verbose = False
