5ea85b8a4c8b772d1e25abed162fa4be1c39fdd7
Jérome Perrin <jerome@nexedi.com>
2019-01-07 00:38:30 +0100

    tidrange: supports giving range as time

--- zodbtools/test/test_tidrange.py
+++ zodbtools/test/test_tidrange.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (C) 2019 Nexedi SA and Contributors.
 #
 # This program is free software: you can Use, Study, Modify and Redistribute
@@ -16,9 +17,36 @@
 # See COPYING file for full licensing terms.
 # See https://www.nexedi.com/licensing for rationale and options.
 
-from pytest import raises
+import datetime
+import os
+import time
 
-from zodbtools.util import parse_tidrange, TidRangeInvalid
+import pytest
+import pytz
+from freezegun import freeze_time
+
+from ZODB.TimeStamp import TimeStamp
+from zodbtools.util import TidRangeInvalid, ashex, parse_tid, parse_tidrange
+
+
+@pytest.fixture
+def fake_time():
+    """Pytest's fixture to run this test as if now() was 2009-08-30T19:20:00Z
+    and if the machine timezone was Europe/Paris
+    """
+    initial_tz = os.environ.get("TZ")
+    os.environ["TZ"] = "Europe/Paris"
+    time.tzset()
+
+    reference_time = datetime.datetime(2009, 8, 30, 19, 20, 0, 0, pytz.utc).astimezone(
+        pytz.timezone("Europe/Paris")
+    )
+    with freeze_time(reference_time):
+        yield
+    del os.environ["TZ"]
+    if initial_tz:
+        os.environ["TZ"] = initial_tz
+    time.tzset()
 
 
 def test_tidrange_tid():
@@ -37,6 +65,28 @@ def test_tidrange_tid():
 
     assert (None, None) == parse_tidrange("..")
 
-    with raises(TidRangeInvalid) as exc:
+    with pytest.raises(TidRangeInvalid) as exc:
         parse_tidrange("invalid")
     assert exc.value.args == ("invalid",)
+
+
+def test_tidrange_date():
+    assert (
+        b"\x03\xc4\x85v\x00\x00\x00\x00",
+        b"\x03\xc4\x88\xa0\x00\x00\x00\x00",
+    ) == parse_tidrange("2018-01-01T10:30:00Z..2018-01-02T00:00:00.000000+00:00")
+
+
+test_parameters = []
+with open(
+    os.path.join(os.path.dirname(__file__), "testdata", "tidrange-formats.txt")
+) as f:
+    for line in f:
+        line = line.strip()
+        if line and not line.startswith("#"):
+            test_parameters.append(line.split(" ", 2))
+
+
+@pytest.mark.parametrize("reference_time,reference_tid,input_time", test_parameters)
+def test_parse_tid_time_format(fake_time, reference_time, reference_tid, input_time):
+    assert reference_tid == ashex(parse_tid(input_time))
